618e6b004b84966381595f716ff0d935dd5911c1
Wanyi <wanyilee0805@gmail.com>
2022-03-03 08:55:27 -0800

    setup app.js in backend

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet'); // Enable better overall security
+const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser()); // parsing cookies
+app.use(express.json()); // parsing JSON bodies of requests with Content-Type of "application/json"
+
+if (!isProduction) {
+  app.use(cors()); // enable cors only in development
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+}))
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true // can't be read by Javascript
+    }
+  })
+)
