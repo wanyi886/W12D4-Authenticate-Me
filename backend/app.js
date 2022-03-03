81b587cb47bcbffe8d63e8ca8d4320c12b96ec61
Wanyi <wanyilee0805@gmail.com>
2022-03-03 09:50:02 -0800

    completed error handling

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet'); // Enable better overall security
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize')
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -38,5 +39,39 @@ app.use(
 )
 
 app.use(routes);
+
+// underscore infront of variables are telling people
+// this is not necessary be used, but syntax needs it
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  // err.errors could be a string, but front end may expect an array
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// formatting all the errors before returning a JSON response
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
 
 module.exports = app;
