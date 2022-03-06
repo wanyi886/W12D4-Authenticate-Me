7e279de95a87e1624809718d7bc84b5bf756a1e7
Wanyi <wanyilee0805@gmail.com>
2022-03-06 09:30:44 -0800

    add SignupForm.css

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,4 +1,6 @@
 // frontend/src/components/SignupFormPage/index.js
+import './SignupForm.css';
+
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
