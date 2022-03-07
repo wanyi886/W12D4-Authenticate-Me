06159d4348f46a3df52f29337cb7a00407a5f2ad
Wanyi <wanyilee0805@gmail.com>
2022-03-06 20:32:23 -0800

    added tickets seed data

--- backend/db/seeders/20220306195106-titcket-data.js
+++ backend/db/seeders/20220306195106-titcket-data.js
@@ -2,25 +2,51 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    return queryInterface.bulkInsert('Tickets', [
+      {
+        eventId: 1,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 16,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 16,
+        userId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 3,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 7,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        eventId: 8,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+
+
+], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('Tickets', null, {});
   }
 };
