92c6db9f87fb99c85759a2b177672620b540c521
Wanyi <wanyilee0805@gmail.com>
2022-03-08 10:53:11 -0800

    change the way grabing event

--- frontend/src/components/EventDetail/index.js
+++ frontend/src/components/EventDetail/index.js
@@ -6,29 +6,24 @@ import { getAllEvents, getOneEvent } from "../../store/event";
 const EventDetail = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
-  const eventList = useSelector(state => state.event.list); // This is an empty array
-  console.log("eventList!!!!!!", eventList);
 
   console.log("eventID", id);
-  const event = eventList.find(event => event.id === Number(id));
+  // const event = eventList.find(event => event.id === Number(id));
 
-  // const event = useSelector(state => state.event[eventId]) Not working
+  const event = useSelector(state => state.event[id])
 
   console.log("event", event);
 
   useEffect(() => {
-
-      dispatch(getAllEvents());
-      dispatch(getOneEvent(id));
-
+    dispatch(getOneEvent(id));
   }, [dispatch])
 
   return (
     <div>
-      <h2>Event Detail Page</h2>
+      <h1>{event.title}</h1>
       <button type="submit">Register</button>
       <ul>
-        {/* <li>{event.id}</li> */}
+        <li>{event.title}</li>
       </ul>
     </div>
   )
