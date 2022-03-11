7a61b145e80eab7aeebd6985d7673e9a04ec87bb
Wanyi <wanyilee0805@gmail.com>
2022-03-10 16:57:16 -0800

    delete feature but need refresh to let it disapear

--- frontend/src/components/MyTickets/index.js
+++ frontend/src/components/MyTickets/index.js
@@ -2,17 +2,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getTickets } from '../../store/ticket'
 import { Link } from "react-router-dom";
+import { deleteTicket } from "../../store/ticket";
 
 const MyTickets = () => {
   const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
   const tickets = useSelector(state => state.ticket.list)
-  
+
   console.log(new Date().toDateString())
 
   useEffect(() => {
     dispatch(getTickets(sessionUser.id))
   }, [dispatch]);
+
+  const handleCancelClick = (e, ticketId) => {
+    e.preventDefault();
+    dispatch(deleteTicket(ticketId));
+  }
 
   let content;
 
@@ -35,6 +41,9 @@ const MyTickets = () => {
                 <div className="id">#{ticket.id}</div>
                 <div className="title">{ticket.Event.title}</div>
                 <div className="date">{new Date(ticket.Event.date).toDateString()}</div>
+                <div>
+                  <button onClick={(e)=> handleCancelClick(e, ticket.id)}>Cancel Registration</button>
+                </div>
               </div>
             )
           })}
